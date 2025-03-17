6410a12fb7b9a78261e72631c23aeb05f04c2a81
albaraa <b.br_1@hotmail.com>
2025-03-17 16:01:48 +0300

    some deployment fixes

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -12,7 +12,7 @@ export default function TemplatesPage() {
   }, []);
 
   return (
-    <DashboardLayout currentPath="/templates" versionNumber={version}>
+    <DashboardLayout currentPath="/templates">
       <div className="space-y-6">
         <div>
           <h1 className="text-3xl font-bold">Templates</h1>
